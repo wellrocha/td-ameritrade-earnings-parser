a44fc7a7b318dcb58761c3a54d48682875f7fd62
Wellington Rocha <wellrocha@gmail.com>
2022-12-14 15:52:28 -0300

    Quando é feito o pagamento de algum provento no dia em que foi extraído o CSV da TD Ameritrade. Ex: Pagamento Provento 14 Dezembro 2022, CSV Extraído 14 Dezembro 2022 O campo SYMBOL vem vazio, quando isso acontecer, o código irá buscar o Symbol no campo Description, além disso, foi necessário ajustar o texto do campo Description para que fique no padrão.

--- src/main/java/com/wellrocha/dtos/Transaction.java
+++ src/main/java/com/wellrocha/dtos/Transaction.java
@@ -5,6 +5,7 @@ import com.opencsv.bean.CsvDate;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.Locale;
 
 public class Transaction {
 
@@ -12,7 +13,7 @@ public class Transaction {
     @CsvDate("MM/dd/yyyy")
     private LocalDate date;
 
-    @CsvBindByName(column = "SYMBOL", required = true)
+    @CsvBindByName(column = "SYMBOL")
     private String symbol;
 
     @CsvBindByName(column = "DESCRIPTION", required = true)
@@ -32,16 +33,31 @@ public class Transaction {
         return symbol;
     }
 
+    public void setSymbol(String symbol) {
+        if (symbol.isEmpty()) {
+            this.symbol= this.getDescription().split(" \\(")[1].replace(")", "");
+        } else {
+            this.symbol = symbol;
+        }
+    }
+
     public String getDescription() {
         return description;
     }
 
-    public BigDecimal getAmount() {
-        return amount;
+    public void setDescription(String description) {
+        if (description.contains("~")) {
+            var descriptionSplit = description.split("~");
+            this.description = String.format("%s (%s)",
+                    descriptionSplit[0].toUpperCase(Locale.ENGLISH),
+                    descriptionSplit[1].toUpperCase(Locale.ENGLISH));
+        } else {
+            this.description = description.toUpperCase(Locale.ENGLISH);
+        }
     }
 
-    public void setDate(LocalDate date) {
-        this.date = date;
+    public BigDecimal getAmount() {
+        return amount;
     }
 
 }
